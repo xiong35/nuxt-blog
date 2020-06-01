287f1d955ba4cf7fcab5abc15ae277a117a3d156
xiong35 <2242787668@qq.com>
2020-06-01 18:09:16 +0800

    import mavon editor

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '@/plugins/vue-mavon-editor', ssr: false }
   ],
   /*
   ** Nuxt.js dev-modules
@@ -78,7 +79,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
