dc3157a1b2d43b3c32882cd86d016cd3938da775
xiong35 <2242787668@qq.com>
2020-06-02 10:13:47 +0800

    footer social

--- components/footer/Contributors.vue
+++ components/footer/Contributors.vue
@@ -2,6 +2,42 @@
 <template>
   <v-col class="pa-4 col-6 col-md-3">
     <h4>Contributor(s)</h4>
+    <v-simple-table class="pa-3 text--secondary">
+      <template v-slot:default>
+        <tbody>
+          <tr>
+            <td>PM</td>
+            <td>
+              <a class="text--secondary" href="http://xiong35.cn">Xiong35</a>
+            </td>
+          </tr>
+          <tr>
+            <td>UI</td>
+            <td>
+              <a class="text--secondary" href="http://xiong35.cn">Xiong35</a>
+            </td>
+          </tr>
+          <tr>
+            <td>前端</td>
+            <td>
+              <a class="text--secondary" href="http://xiong35.cn">Xiong35</a>
+            </td>
+          </tr>
+          <tr>
+            <td>后端</td>
+            <td>
+              <a class="text--secondary" href="http://xiong35.cn">Xiong35</a>
+            </td>
+          </tr>
+          <tr>
+            <td>运维</td>
+            <td>
+              <a class="text--secondary" href="http://xiong35.cn">Xiong35</a>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-col>
 </template>
 
--- components/footer/Social.vue
+++ components/footer/Social.vue
@@ -1,7 +1,31 @@
 // dependency: //
 <template>
   <v-col class="pa-4 col-6 col-md-3">
+    <span id="qq-num" class="d-none">22427</span>
     <h4>Social</h4>
+    <v-layout row wrap class="d-flex pa-4 justify-space-around">
+      <v-btn href="https://github.com/xiong35" icon color="grey darken-2">
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+      <v-btn icon color="grey darken-2" @click="copy('q')">
+        <v-icon>mdi-qqchat</v-icon>
+      </v-btn>
+      <v-btn icon color="grey darken-2" @click="copy('v')">
+        <v-icon>mdi-wechat</v-icon>
+      </v-btn>
+    </v-layout>
+    <v-snackbar top v-model="snackBarQ" color="success" :timeout="1400">
+      已复制QQ号到剪切板😉
+      <v-btn dark text @click="snackBarQ = false">
+        Close
+      </v-btn>
+    </v-snackbar>
+    <v-snackbar top v-model="snackBarV" color="success" :timeout="1400">
+      已复制微信号到剪切板😉
+      <v-btn dark text @click="snackBarv = false">
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-col>
 </template>
 
@@ -10,11 +34,26 @@
     name: "Social",
     components: {},
     data() {
-      return {};
+      return {
+        snackBarQ: false,
+        snackBarV: false,
+      };
     },
     computed: {},
     watch: {},
-    methods: {},
+    methods: {
+      copy(type) {
+        let number = "";
+        if (type === "q") {
+          this.snackBarQ = true;
+          number = "2242787668";
+        } else if (type === "v") {
+          this.snackBarV = true;
+          number = "xyl2242787668";
+        }
+        navigator.clipboard.writeText(number);
+      },
+    },
     created() {},
     mounted() {},
   };
