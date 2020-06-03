5e0762b65cf1706fdd7e0cf62ae8b625d9b91f89
xiong35 <2242787668@qq.com>
2020-06-03 09:29:15 +0800

    ready to release

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'xiong35 的博客里的一些内容' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -53,6 +53,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    progress: true
   },
   /*
   ** vuetify module configuration
