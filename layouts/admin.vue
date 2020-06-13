6739d957571ed75bb83c14859f794c3e28cbf788
xiong35 <2242787668@qq.com>
2020-06-13 10:57:48 +0800

    change admin bar

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -2,12 +2,13 @@
 <template>
   <v-app>
     <v-app-bar flat app>
-      <v-btn @click="$router.push('/')" color="grey darken-2" text>
-        <v-icon left>mdi-arrow-left-bold-box-outline</v-icon>BACK
-      </v-btn>
       <v-toolbar-title class="font-weight-thin grey--text text--darken-2">
         🧸 Xiong<sup>35 </sup>'s Blog Admin
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="$router.push('/')" color="grey darken-2" text>
+        <v-icon left>mdi-logout-variant mdi-rotate-180</v-icon>BACK
+      </v-btn>
     </v-app-bar>
     <v-content class="pa-md-10 mt-9 pa-5 grey text-center lighten-4">
       <nuxt />
